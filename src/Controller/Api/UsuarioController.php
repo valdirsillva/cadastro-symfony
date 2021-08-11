2e940d6ec9c7af544d28e7043de30b74623b0243
Valdir Silva <valdirpiresba@gamil.com>
2021-08-11 18:56:29 -0300

    cadastrando um usuario no banco de dados via api

--- src/Controller/Api/UsuarioController.php
+++ src/Controller/Api/UsuarioController.php
@@ -7,6 +7,9 @@ namespace App\Controller\Api;
 use App\Entity\Usuario;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -31,8 +34,24 @@ class UsuarioController extends AbstractController
     /**
      * @Route("/cadastra", methods={"POST"}, name="cadastra")
      */
-    public function cadastra(): JsonResponse 
+    public function cadastra(Request $request): Response
     {
-        return new JsonResponse(['implementar cadastro da api'], 404);
+        $data = $request->request->all();
+        
+        $usuario = new Usuario;
+        $usuario->setNome($data['nome']);
+        $usuario->setEmail($data['email']);
+
+        $doctrine = $this->getDoctrine()->getManager();
+        $doctrine->persist($usuario);
+        $doctrine->flush();   
+
+        if ( $doctrine->contains($usuario) ) {
+
+            return  new JsonResponse("ok", 200);
+
+        } else {
+           return  new JsonResponse("error", 404);
+        }        
     }
 }
